f53ef46d4f630fc98909ab536be461a3d607729e
yoelalf <yoelalfandi@gmail.com>
2023-07-03 00:48:09 +0700

    chessboard more concise solutions

--- script.js
+++ script.js
@@ -64,7 +64,22 @@ ChessBoard = () =>{
         }
         console.log(hash.join(" "))
         hash = []
-        
     }  
+}
 
+//More Concise Solutions to Chessboard
+ChessBoard1 = () =>{
+    var size = 8; //this is the variable setting
+
+    var board = "";//this is the empty string we're going to add either ' ' , '#' or newline
+
+    for (var y = 0; y < size; y++) {   /*in the outer loop we add newline to seperate rows*/
+    for (var x = 0; x < size; x++) {/*every inner loop rappresents a line, and alternatively it's adding either ' ' or '#' to the string that's being populated*/
+        if ((x + y) % 2 == 0)
+        board += " ";
+        else
+        board += "#";
+    }
+    board += "\n";
+    }
 }
